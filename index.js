3d61e5e885afc5d8e35daf47dac6f81548edd8bd
majjikishore007 <majjikishore031@gmail.com>
2022-01-09 17:04:19 +0530

    setting up db

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const app = express()
 
 
 const PORT = process.env.PORT || 8080;
+const DB_URL = process.env.DB_URL;
 
 //Routes
-const memoizeRoutes =require('./routes')
+const memoizeRoutes = require("./routes");
 app.use("/", memoizeRoutes);
 
-//server 
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("💚 DB IS CONNECTED");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// server startup
+
 app.listen(PORT, () => {
-    console.log("Server is listening on port " + PORT);
-})
+  console.log(`💚 💙 💛 app is  listening on ${PORT}`);
+});
